a9d604fb27ddea27bec9ec9cac21d58dd7564ccc
rwu823 <rocky823@gmail.com>
2013-10-02 23:08:44 +0800

    update home.js fix js error

--- blog/home.js
+++ blog/home.js
@@ -1,22 +1,5 @@
 define(function(require, exports, module){
 
-  var Blog = angular.module('Blog', [])
-
-  Blog.config( function( $routeProvider ){
-    $routeProvider
-      // .when('/:year/:month/:day', {
-      //   template : 'hello'
-      // })
-      .when('/pizza', {
-        template : 'This is my App'
-
-      })
-
-  })
-
-  Blog.controller('aa', function($location){
-    // $location.path('/pizza')
-  });
-
+  
 
 })
